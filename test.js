9345ff6832e6c83b78adf6324920b4b106445097
kiensamson0000 <kienkh99@gmail.com>
2022-03-31 08:04:04 +0700

    update code

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
 const logger = require('./logger');
 
-throw new Error('Test loi');
+// throw new Error('Test loi');
+
+
+logger.info('Info log %');
+logger.warn('Warning log');
+logger.error(new Error('Error log'));
+
+
